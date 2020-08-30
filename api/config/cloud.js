1a2df51fb5c3f7692db53a48de6bb13cba9cfce0
Tom Rizzi <tcr59@uclive.ac.nz>
2020-08-30 12:28:34 +1200

    Changed how google bucket key was stored

--- api/config/cloud.js
+++ api/config/cloud.js
@@ -1,7 +1,8 @@
 const Cloud = require('@google-cloud/storage');
 const path = require('path');
 const keyPath = process.env.BUCKET_KEY_PATH;
-const serviceKey = path.join(__dirname, `../${keyPath}`);
+// const serviceKey = path.join(__dirname, `../${keyPath}`);
+const serviceKey = process.env.BUCKET_KEY;
 
 const { Storage } = Cloud;
 const storage = new Storage({
