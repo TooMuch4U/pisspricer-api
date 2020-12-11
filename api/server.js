9e093b5872a7344f7b69aed12462a9decec0f222
Tom Rizzi <tcr59@uclive.ac.nz>
2020-12-11 20:55:24 +1300

    Added https functionality

--- api/server.js
+++ api/server.js
@@ -1,9 +1,13 @@
 require('dotenv').config();
 const db = require('./config/db');
 const express = require('./config/express');
+const http = require('http');
+const https = require('https');
+const fs = require('fs');
 
 const app = express();
 const port = process.env.OPEN_PORT || 8080;
+const httpsPort = process.env.OPEN_PORT_HTTPS;
 
 // Test connection to MySQL on start-up
 async function testDbConnection() {
@@ -16,9 +20,27 @@ async function testDbConnection() {
     }
 }
 
+// TEst connection then connect
 testDbConnection()
     .then(function () {
-        app.listen(port, function () {
-            console.log(`Listening on port: ${port}`);
+
+        // Start http
+        let server = http.createServer(app).listen(port, function() {
+            console.log(`HTTP listening on port: ${port}`);
         });
+
+        // Check if should start https
+        if (httpsPort) {
+            // Get cert files
+            let sslOptions = {
+                key: fs.readFileSync('key.pem'),
+                cert: fs.readFileSync('cert.pem')
+            };
+
+            // Start https server
+            let serverHttps = https.createServer(sslOptions, app).listen(httpsPort, function () {
+                console.log(`HTTPS listening on port: ${httpsPort}`);
+            });
+        }
+
     });
