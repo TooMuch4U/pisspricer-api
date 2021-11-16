c64a42ab3dcd8fc6b9b5a172c65aad0157a340e6
Tom Rizzi <t.rizzi@icloud.com>
2021-11-16 23:16:44 +1300

    Added check to see if we are connecting VIA cloud sql or normal connection

--- api/config/db.js
+++ api/config/db.js
@@ -13,17 +13,31 @@ let pool = null;
 // };
 
 exports.createPool = async config => {
-  const dbSocketPath = process.env.DB_SOCKET_PATH || '/cloudsql';
+  // check if we are connecting to cloud sql or not
+  if (process.env.CLOUD_SQL_CONNECTION_NAME) {
+    const dbSocketPath = process.env.DB_SOCKET_PATH || '/cloudsql';
 
-  // Establish a connection to the database
-  pool = mysql.createPool({
-    multipleStatements: true,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE,
-    // If connecting via unix domain socket, specify the path
-    socketPath: `${dbSocketPath}/${process.env.CLOUD_SQL_CONNECTION_NAME}`
-  });
+    // Establish a connection to the database
+    pool = mysql.createPool({
+      multipleStatements: true,
+      user: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
+      // If connecting via unix domain socket, specify the path
+      socketPath: `${dbSocketPath}/${process.env.CLOUD_SQL_CONNECTION_NAME}`
+    });
+  }
+  else {
+    pool = mysql.createPool({
+        multipleStatements: true,
+        host: process.env.MYSQL_HOST,
+        user: process.env.MYSQL_USER,
+        password: process.env.MYSQL_PASSWORD,
+        database: process.env.MYSQL_DATABASE,
+        port: process.env.MYSQL_PORT || 3306
+    });
+  }
+    
 };
 
 exports.getPool = function () {
