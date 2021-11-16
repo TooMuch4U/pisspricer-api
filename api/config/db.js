e801a646ecb8e62447401d4eba022271c5e88cab
Tom Rizzi <t.rizzi@icloud.com>
2021-11-16 23:09:41 +1300

    Update db.js

--- api/config/db.js
+++ api/config/db.js
@@ -1,15 +1,29 @@
 const mysql = require('promise-mysql');
 
 let pool = null;
-exports.createPool = async function () {
-    pool = mysql.createPool({
-        multipleStatements: true,
-        host: process.env.MYSQL_HOST,
-        user: process.env.MYSQL_USER,
-        password: process.env.MYSQL_PASSWORD,
-        database: process.env.MYSQL_DATABASE,
-        port: process.env.MYSQL_PORT || 3306
-    });
+// exports.createPool = async function () {
+//     pool = mysql.createPool({
+//         multipleStatements: true,
+//         host: process.env.MYSQL_HOST,
+//         user: process.env.MYSQL_USER,
+//         password: process.env.MYSQL_PASSWORD,
+//         database: process.env.MYSQL_DATABASE,
+//         port: process.env.MYSQL_PORT || 3306
+//     });
+// };
+
+exports.createPool = async config => {
+  const dbSocketPath = process.env.DB_SOCKET_PATH || '/cloudsql';
+
+  // Establish a connection to the database
+  pool = mysql.createPool({
+    multipleStatements: true,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    // If connecting via unix domain socket, specify the path
+    socketPath: `${dbSocketPath}/${process.env.CLOUD_SQL_CONNECTION_NAME}`
+  });
 };
 
 exports.getPool = function () {
